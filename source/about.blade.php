f4506d84c9005071b8ec5f938bef92da29929169
rimji <88181976+rimji@users.noreply.github.com>
2021-08-04 15:15:43 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,14 +3,7 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
-
-    <p>My name is {{ $page->owner->name }}</p>
-
-    <h2>Links:</h2>
-
-    <ul>
-        <li><a href="https://twitter.com/{{ $page->owner->twitter }}" target="_blank">Twitter</a></li>
-        <li><a href="https://github.com/{{ $page->owner->github }}" target="_blank">GitHub</a></li>
-    </ul>
+   <small><center>
+   We love and sell POGOBANDS !
+   </center> </small>
 @endsection
