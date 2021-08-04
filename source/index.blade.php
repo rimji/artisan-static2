512b41405177e0e7495ddab1690562b4bd278b0a
rimji <88181976+rimji@users.noreply.github.com>
2021-08-04 15:13:02 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,21 +1,8 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static</h1>
-
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
-
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
-
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
-    </a>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+    <center>
+    <p><img src="https://user-images.githubusercontent.com/88181976/127647479-d1434cbf-fe29-46bb-9c17-28ec743ac00f.jpg" alt="HTML" width="640" height="427"></p>
+    <small><center><a href="https://www.rimji.com/about/">About</a> &nbsp &nbsp <a href="https://www.rimji.com/contact/">Contact</a></small>
+    </center>
 @endsection
