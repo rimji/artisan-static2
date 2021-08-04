5d8750d34b5e4fa79c5cf1ba0ae364fe468ada83
rimji <88181976+rimji@users.noreply.github.com>
2021-08-04 15:31:30 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav>
-               <strong><a href="https://www.pogoband.com/">じ Pogoband</a></strong><br>
+               <strong><a href="https://www.pogoband.com/">じ POGOBAND</a></strong><br>
             </nav>
         </header>
 
