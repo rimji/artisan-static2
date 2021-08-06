4d6ec46e874bcc3071f299d26795e52b788fe70d
rimji <88181976+rimji@users.noreply.github.com>
2021-08-06 09:33:55 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav>
-               <strong>じ <a href="https://www.cocomeo.com/">COCOMEO</a></strong><br>
+               <strong>じ <a href="https://www.cocomeo.com/">CoCoMeo</a></strong><br>
             </nav>
         </header>
 
@@ -32,7 +32,7 @@
 
         <footer>
              <small><center>
-             &copy; 2021 Cocomeo
+             &copy; 2021 CoCoMeo
              </center></small>
         </footer>
     </section>
