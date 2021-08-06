fa77afe12dc98f357a8d35d19e4e8c0a387a3e1c
rimji <88181976+rimji@users.noreply.github.com>
2021-08-06 16:08:46 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <center>
-    <p><img src="https://user-images.githubusercontent.com/88181976/127647479-d1434cbf-fe29-46bb-9c17-28ec743ac00f.jpg" alt="HTML" width="640" height="427"></p>
+    <p></p>
     <small><center><a href="https://www.cocomeo.com/about/">About</a> &nbsp &nbsp <a href="https://www.cocomeo.com/contact/">Contact</a></small>
     </center>
 @endsection
