b660ce4b8eadb0a367417c467bace8f3935095f9
rimji <88181976+rimji@users.noreply.github.com>
2021-08-06 17:43:08 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <center>
-    <p>SCRUNCHIES <i>!<i></p>
+    <p><i>SCRUNCHIES !</i></p>
     <small><center><a href="https://www.cocomeo.com/about/">About</a> &nbsp &nbsp <a href="https://www.cocomeo.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/cocomeo.official/">Instagram</a></small>
     </center>
 @endsection
