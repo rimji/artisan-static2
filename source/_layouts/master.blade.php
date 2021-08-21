27191bf30abbece761c6d91e59e99de01716a048
rimji <88181976+rimji@users.noreply.github.com>
2021-08-21 19:35:02 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -33,7 +33,7 @@
         <footer>
              <small><center>
              <a href="https://www.pollki.com/about/">About</a> &nbsp &nbsp <a href="https://www.pollki.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/pollki/">Instagram</a><br>
-              copy; 2021
+             &copy; 2021
              </center></small>
         </footer>
     </section>
