1e19f978de1b0a9b3fe762d173f6515ccfa637c3
rimji <88181976+rimji@users.noreply.github.com>
2021-08-21 16:33:12 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <center>
-    <small><center><a href="https://www.pollki.com/about/">About</a> &nbsp &nbsp <a href="https://www.pollki.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/pollki/">Instagram</a></small>
+     
     </center>
 @endsection
