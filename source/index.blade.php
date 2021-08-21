d9323a577f6eab502a374bd1652212aee3911d38
rimji <88181976+rimji@users.noreply.github.com>
2021-08-21 16:35:19 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,9 @@
 @extends('_layouts.master')
 
 @section('content')
-    <center>
-     
-    </center>
+    <center><small>
+     Scrunchies
+     No hair damage
+     Made with ❤️ in India
+     </small> </center>
 @endsection
