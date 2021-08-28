2fa008c7f91254319128c42ce79eb3182ea30ba2
rimji <88181976+rimji@users.noreply.github.com>
2021-08-28 22:16:20 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
    <small><center>
-   We love and sell comfortable and high quality hair scrunchies, made with ❤️.
+   We love and sell comfortable and high quality hair scrunchies. <br>Made with ❤️
    </center> </small>
 @endsection
