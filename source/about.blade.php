06b7a193fbde682aa33240cf41835e25b634b5a8
rimji <88181976+rimji@users.noreply.github.com>
2021-08-28 22:19:27 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
    <small><center>
-   Comfortable and high quality hair scrunchies. <br>Made with ❤️
+   Comfortable and high quality hair scrunchies.<br>No hair damage.<br>Made with ❤️
    </center> </small>
 @endsection
